aa39b5eafd48f270595ea476e9cbae910bc86691
Sean T Allen <sean@monkeysnatchbanana.com>
2010-10-25 12:03:19 -0400

    Removed rails example until rails integration issues are worked out.

--- ruby/examples/blog_rails/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BlogRails::Application.config.secret_token = '7bd6d99096ecc3161558e7d24aee019da2b2585f31e9d4343b01368c176181a74fb2faccf3a3356c8f088f35ec674d795db8e5f91d713941c9e2bcfa26dcddab'
