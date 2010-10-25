69e714b1a116a0bb514c48a32be45a1d4d7b2fe2
Sean T Allen <sean@monkeysnatchbanana.com>
2010-10-25 12:31:28 -0400

    Added 'no layout' sinatra full example.
    Shows how you can take complete application pages that don't use a layout ( like out new example ).
    And with a couple lines of code, wrap the entire thing into a layout.
    
    Need to run by James and David so I can get their response to questions like:
      Should this be broken up into different examples for clarity?
      Should it be kept in same example directory but use less complicated templates for
       add and list when doing layout example?

---
+++ ruby/examples/todo_sinatra/full/todo-no-layout.rb
@@ -0,0 +1,55 @@
+require 'rubygems'
+require 'sinatra'
+require 'dm-core'
+require 'dm-validations'
+require 'dm-migrations'
+require 'sanitize'
+require 'nate'
+
+# Datamapper setup
+db_temp_file = Tempfile.new( "nate_todo_db" )
+DataMapper.setup( :default, "sqlite://#{db_temp_file.path}")
+
+class ToDo
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :title,      String
+  property :created_at, DateTime
+  property :complete,   Boolean, :default=>false
+
+  validates_presence_of :title
+end
+
+DataMapper.finalize
+DataMapper.auto_migrate!
+
+# templates
+List    = Nate::Engine.from_file 'templates/list.html'
+AddToDo = Nate::Engine.from_file 'templates/add.html' 
+
+# controllers
+get '/' do
+  todos = ToDo.all( :complete => false )
+  todo_data = todos.collect do |todo|
+    { '.title' => todo.title,  'input[@name=id]' => { 'value' => todo.id }}
+  end 
+  data = { '.todo' => todo_data }
+  
+  List.inject_with( '.todolist' => data ).render
+end
+
+get '/new' do
+  AddToDo.render
+end
+
+post '/add' do
+  ToDo.create( :title => Sanitize.clean( params[:title] ), :created_at => Time.now )
+  redirect '/'
+end
+
+post '/finished' do
+  todo = ToDo.get( params[:id] )
+  todo.update( :complete => true )
+  redirect '/'
+end
